d808c5852ca6b984b579675c5a85e0e72d2f1f3b
KILLER.CODES <Killercodes@users.noreply.github.com>
2023-08-11 15:33:21 +0530

    Update DateExtended.cs

--- Date/DateExtended.cs
+++ Date/DateExtended.cs
@@ -4,29 +4,29 @@ namespace nExtensions
 {
     public static class DateExtention
     {
-		/// <summary>
-		/// Convert to EPOCH
-		/// </summary>
-		/// <returns>long EPOCH</returns>
+	/// <summary>
+	/// Convert to EPOCH
+	/// </summary>
+	/// <returns>long EPOCH</returns>
         public static long ToEpoch (this DateTime value)
         {
             long epoch = (long)(value.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
             return epoch;
         }
 
-		/// <summary>
-		/// Convert from EPOCH to Datetime
-		/// </summary>
-		/// <returns>object DateTime</returns>
-        private static DateTime EpochToString (this long epoch)
+	/// <summary>
+	/// Convert from EPOCH to Datetime
+	/// </summary>
+	/// <returns>object DateTime</returns>
+        private static DateTime FromEpoch (this long epoch)
         {
             DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(epoch);
             return dateTime;
         }
 
-		private static string ToGMT (this DateTime dateTime)
-		{
-			return dateTime.ToString("R");
-		}
+	private static string ToGMT (this DateTime dateTime)
+	{
+		return dateTime.ToString("R");
+	}
     }
 }
